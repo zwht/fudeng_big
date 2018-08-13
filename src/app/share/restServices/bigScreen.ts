e73f9dbacf4040ceb50408db0a14c3b58db72e07
zhengxianshan <605186386@qq.com>
2018-08-13 10:15:55 +0800

    连接服务器

---
+++ src/app/share/restServices/bigScreen.ts
@@ -0,0 +1,68 @@
+/**
+ * Created by zhaowei on 2017/10/16.
+ */
+import { Injectable } from '@angular/core';
+import { HttpServer } from '../services/HttpServer';
+@Injectable({
+    providedIn: 'root'
+})
+export class bigScreenService {
+
+    url = '/v1/bigscreen/:params1/:params2/:params3/:params4/:params5';
+    urls = {
+        add: {
+            method: 'post',
+            params: {
+                params1: 'add'
+            }
+        },
+        basic: {
+            method: 'get',
+            params: {
+                params1: 'basic'
+            }
+        },
+        dailyLoanAmountQuery: {
+            method: 'get',
+            params: {
+                params1: 'dailyLoanAmountQuery'
+            }
+        },
+        dailyloanbalanceQuery: {
+            method: 'get',
+            params: {
+                params1: 'dailyloanbalanceQuery'
+            }
+        },
+        fetchaPage: {
+            method: 'get',
+            params: {
+                params1: 'fetchaPage'
+            }
+        },
+        loanbalanceQuery: {
+            method: 'get',
+            params: {
+                params1: 'loanbalanceQuery'
+            }
+        },
+        tolannumberofloancustomersQuery: {
+            method: 'get',
+            params: {
+                params1: 'tolannumberofloancustomersQuery'
+            }
+        },
+        totalloanamountQuery: {
+            method: 'get',
+            params: {
+                params1: 'totalloanamountQuery'
+            }
+        },
+
+    };
+
+    constructor(httpServer: HttpServer) {
+        httpServer.S(this);
+    }
+
+}
