ee24fa7ce941ef781512aa69344c9b19d0a5fa07
zhengxianshan <605186386@qq.com>
2018-08-14 10:24:14 +0800

    提交

--- src/app/bigScreen/components/province/pr-chart2/pr-chart2.component.ts
+++ src/app/bigScreen/components/province/pr-chart2/pr-chart2.component.ts
@@ -126,7 +126,6 @@ export class PrChart2Component implements OnInit {
         {
           name: '邮件营销',
           type: 'line',
-          stack: '总量',
           data: [120, 132, 101, 134, 90, 230, 210],
           symbol: 'none',
           smooth: true,
@@ -134,7 +133,6 @@ export class PrChart2Component implements OnInit {
         {
           name: '联盟广告',
           type: 'line',
-          stack: '总量',
           data: [220, 182, 191, 234, 290, 330, 310],
           symbol: 'none',
           smooth: true,
@@ -142,7 +140,6 @@ export class PrChart2Component implements OnInit {
         {
           name: '视频广告',
           type: 'line',
-          stack: '总量',
           data: [150, 232, 201, 154, 190, 330, 410],
           symbol: 'none',
           smooth: true,
@@ -150,7 +147,6 @@ export class PrChart2Component implements OnInit {
         {
           name: '直接访问',
           type: 'line',
-          stack: '总量',
           data: [320, 332, 301, 334, 390, 330, 320],
           symbol: 'none',
           smooth: true,
@@ -158,7 +154,6 @@ export class PrChart2Component implements OnInit {
         {
           name: '搜索引擎',
           type: 'line',
-          stack: '总量',
           data: [820, 932, 901, 934, 1290, 1330, 1320],
           symbol: 'none',
           smooth: true,
